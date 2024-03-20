74d237fe666101faefc28eb7f7124cdb8a06c345
Zahid Afsar <zahidtempz@icloud.com>
2024-03-20 14:45:38 -0700

    first commit

---
+++ Controllers/MatchController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace strikeshowdown_backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MatchController : ControllerBase
+    {
+        
+    }
+}
