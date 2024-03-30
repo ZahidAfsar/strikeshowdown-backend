5ca36e652b864ad3a6586294f291462bd9d8d832
Zahid Afsar <zahidtempz@icloud.com>
2024-03-30 14:49:18 -0700

    first commit 2.1

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddDbContext<DataContext>(Options => Options.UseSqlServer(conne
 
 builder.Services.AddCors(options => options.AddPolicy("StrikePolicy", 
 builder => {
-    builder.WithOrigins("http://localhost:5039")
+    builder.WithOrigins("http://localhost:5039", "https://strikeshowdownbackend.azurewebsites.net", "http://localhost:3000")
     .AllowAnyHeader()
     .AllowAnyMethod();
 }));
