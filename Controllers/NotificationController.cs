688b961ecba672557708a7a17c3236ee3e80aef6
JayvonK <jaykhuth@gmail.com>
2024-05-15 04:37:06 -0700

    version 5.01

---
+++ Controllers/NotificationController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using strikeshowdown_backend.Models.DTO;
+using strikeshowdown_backend.Services;
+
+namespace strikeshowdown_backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NotificationController : ControllerBase
+    {
+        private readonly NotificationService _noti;
+        public NotificationController(NotificationService noti)
+        {
+            _noti = noti;
+        }
+
+        [HttpGet]
+        [Route("GetNotificationsByUserID/id")]
+        public IEnumerable<NotificationModel> GetNotificationsByUserID(int id){
+            return _noti.GetNotificationsByUserID(id);
+        }
+
+        [HttpPost]
+        [Route("CreateNotification")]
+        public bool CreateNotification(CreateNotificationDTO noti){
+            return _noti.CreateNotification(noti);
+        }
+
+        [HttpPut]
+        [Route("DeleteNotification")]
+        public bool DeleteNotification(NotificationModel noti){
+            return _noti.DeleteNotification(noti);
+        }
+
+    }
+}
---
+++ Migrations/20240515113635_init.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace strikeshowdown_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MatchInfo",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserID = table.Column<int>(type: "int", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsVisible = table.Column<bool>(type: "bit", nullable: true),
+                    State = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Locations = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Date = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Time = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MaxPpl = table.Column<int>(type: "int", nullable: true),
+                    CurrentPpl = table.Column<int>(type: "int", nullable: true),
+                    MatchUsersIDs = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsFinished = table.Column<bool>(type: "bit", nullable: true),
+                    Publisher = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Wins = table.Column<int>(type: "int", nullable: true),
+                    Average = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Style = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Streak = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MatchInfo", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NotificationInfo",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SenderID = table.Column<int>(type: "int", nullable: false),
+                    SenderUsername = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RecieverID = table.Column<int>(type: "int", nullable: false),
+                    RecieverUsername = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsRead = table.Column<bool>(type: "bit", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NotificationInfo", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RecentWinnerInfo",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserID = table.Column<int>(type: "int", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProfileImage = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Pronouns = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Wins = table.Column<int>(type: "int", nullable: false),
+                    Losses = table.Column<int>(type: "int", nullable: false),
+                    Style = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MainCenter = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Average = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Earnings = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HighGame = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HighSeries = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    Date = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RecentWinnerInfo", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserInfo",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Salt = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Hash = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SecurityQuestion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SecurityQuestionTwo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SecurityQuestionThree = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SecuritySalt = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SecuritySaltTwo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SecuritySaltThree = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SecurityHash = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SecurityHashTwo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SecurityHashThree = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProfileImage = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Pronouns = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Wins = table.Column<int>(type: "int", nullable: false),
+                    Losses = table.Column<int>(type: "int", nullable: false),
+                    Style = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MainCenter = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Average = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Earnings = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HighGame = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HighSeries = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Streak = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserInfo", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MatchInfo");
+
+            migrationBuilder.DropTable(
+                name: "NotificationInfo");
+
+            migrationBuilder.DropTable(
+                name: "RecentWinnerInfo");
+
+            migrationBuilder.DropTable(
+                name: "UserInfo");
+        }
+    }
+}
